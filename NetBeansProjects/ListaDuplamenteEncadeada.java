8462a8346b78b6eeb533e357eb2f9cc8d4fd47d2
Jocas04 <jattanasio57@gmail.com>
2026-04-16 10:09:03 -0300

    lista duplamente encadeada

---
+++ NetBeansProjects/ListaDuplamenteEncadeada.java
@@ -0,0 +1,31 @@
+package listaduplamenteencadeada;
+import java.util.Scanner;
+
+class NO{
+    int valor;
+    NO proximo;
+    NO anterior;
+    
+    NO(int valor){
+        this.valor=valor;
+        this.proximo=null;
+        this.anterior=null;
+    }
+}
+class ListaDupla{
+    private NO inicio;
+    private NO fim;
+    public boolean ListaVazia()  {
+            return inicio==null;
+        }
+    }
+public class ListaDuplamenteEncadeada {
+    
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+       int opcao;
+       int valor;
+       
+    }
+    
+}
