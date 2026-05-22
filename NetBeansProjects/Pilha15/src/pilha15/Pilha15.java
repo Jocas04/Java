dc0d307105d9afebb08188da8e21e94a91e8dad4
Jocas04 <jattanasio57@gmail.com>
2026-05-22 10:07:39 -0300

    Pilha 15
    a pilha 15 verifica se há algum numero maior ao lado direito na string, se nao houver resulta em -1 mas se tiver mostra o numero maior

---
+++ NetBeansProjects/Pilha15/src/pilha15/Pilha15.java
@@ -0,0 +1,32 @@
+package pilha15;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+
+public class Pilha15 {
+    public static int[] NGE(int[] arr) {
+        int n= arr.length;
+        int[] resultado = new int[n];
+        Arrays.fill(resultado, -1);//inicializa com -1 e se nao tiver um numero maior aparecera -1
+        
+        Stack<Integer> Pilha = new Stack<>(); //criacao da pilha
+       
+        for (int i = 0; i < n; i++) {//adiciona +1 a i se for menor qque n
+            while (!Pilha.isEmpty() && arr[i] > arr[Pilha.peek()]) {    
+                // Enquanto pilha não estiver vazia e o elemento atual for maior
+                int indice = Pilha.pop();   //retira o indice e recebe o atual
+                resultado[indice] = arr[i];
+            }
+            Pilha.push(i);  //empilha o indice
+        }
+        return resultado;
+    } 
+    
+    public static void main(String[] args) {
+        int[]entrada={2,7,6,3,1,4,7};
+        int[] resultado = NGE(entrada);
+        System.out.println(Arrays.toString(resultado));//
+    }
+    
+}
