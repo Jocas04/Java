0b0bda965379bd3d503aedc2c65bfb01ea18fa16
Jocas04 <jattanasio57@gmail.com>
2026-05-25 10:17:37 -0300

    Pilha 16
    é parecida com a anterior entretanto agora em vez de imprimir o próximo número maior o sistema imprimi quantas casas são percorridas até chegar em um número maior

---
+++ Pilha16/src/pilha16/Pilha16.java
@@ -0,0 +1,31 @@
+package pilha16;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+
+public class Pilha16 {
+    public static int[] temperatura(int[] temps) {
+        int n = temps.length;   // cria os ints que seram usados para saber a casa e resposta
+        int[] resposta = new int[n];
+        
+        Stack<Integer> Pilha = new Stack<>();//cria a pilha que irá guardar os índices
+        
+        for (int i = 0; i<n;i++) {
+            //enquanto a pilha nao estiver vazia e a temperatura do indice for maior do que a do topo
+            //irá ser retirado da pilha e recebido pelo indice e assim colocado na resposta o indice "i" menos o "indice"
+            while(!Pilha.isEmpty() && temps[i] > temps[Pilha.peek()]) {
+                int indice = Pilha.pop();
+                resposta[indice] = i - indice;
+            }
+            Pilha.push(i);//imprime o indice atual resposta
+        }
+        return resposta;//resultado
+    }
+    public static void main(String[] args) {
+        int[] entrada= {56, 82, 65, 92, 53, 96, 42, 64, 98};
+        int[] resultado= temperatura(entrada);
+        System.out.println(Arrays.toString(resultado)); //
+    }
+    
+}
